aebafac3ce22c63bbe6af37818b22fc0bebafd67
Dalilah Aguilar <dalilahaguilar9@gmail.com>
2021-08-09 11:31:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;  
+
+
+
+// shortGreeting
+      //4) contains "Welcome, "
+     //5) contains the first initial of the name stored in the 'currentUser' variable
+     // 6) ends with an exclamation point
+
